98f63d5854a88b112cf50489f0d299d2d99af950
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-01-22 21:21:26 -0300

    chore: refactor the inital handle and basic structure 2

--- src/main/java/com/bot/spider/controllers/TelegramBotController.java
+++ src/main/java/com/bot/spider/controllers/TelegramBotController.java
@@ -1,8 +1,10 @@
 package com.bot.spider.controllers;
 
 import com.bot.spider.dtos.TelegramMessageDTO;
+import com.bot.spider.enums.TelegramChatAction;
+import com.bot.spider.services.HandlingInputs;
 import com.bot.spider.services.HttpClientService;
-import com.bot.spider.services.SendHello;
+import com.bot.spider.services.TelegramSenders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,16 +31,13 @@ public class TelegramBotController {
     public ResponseEntity<String> webhook(@RequestBody TelegramMessageDTO body) {
         logger.info(String.valueOf(body));
 
-        assert body.message() != null;
-        Long chatId = body.message().chat().id();
-        String text = body.message().text();
-
         try {
-            SendHello sendHello = new SendHello(httpClientService);
-            sendHello.execute(chatId);
+            HandlingInputs handlingInputs = new HandlingInputs(body, httpClientService);
+            handlingInputs.handle();
+
             return new ResponseEntity<>("ok", HttpStatus.OK);
         } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error sending message to chatId: " + chatId);
+            logger.log(Level.SEVERE, "Error : " + e.getMessage(), e);
             return new ResponseEntity<>("error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- src/main/java/com/bot/spider/libs/telegram/TelegramApi.java
+++ src/main/java/com/bot/spider/libs/telegram/TelegramApi.java
@@ -1,21 +1,36 @@
 package com.bot.spider.libs.telegram;
 
-import com.bot.spider.interfaces.TelegramApiMethod;
+import com.bot.spider.enums.TelegramChatAction;
 import com.bot.spider.services.HttpClientService;
 
-public class SendMessage implements TelegramApiMethod {
-    private final Long chatId;
+public class TelegramApi   {
+    private  HttpClientService httpClientService;
 
-    public SendMessage(Long chatId) {
-        this.chatId = chatId;
+    public  TelegramApi( HttpClientService httpClientService) {
+        this.httpClientService = httpClientService;
     }
 
-    @Override
-    public void execute(HttpClientService httpClientService) {
+    public  void sendMessage(String jsonBody) {
         String method = "sendMessage";
+        sendRequest(method, jsonBody);
+    }
+    public  void sendChatAction(long chat_id, TelegramChatAction action) {
+        String method = "sendChatAction";
 
-        String jsonBody = String.format("{\"chat_id\":\"%s\",\"text\":\"Olá! Esta é uma resposta do bot.\"}", this.chatId);
-
+        String jsonBody = String.format("{\"chat_id\":\"%s\",\"action\":\"%s\"}", chat_id, action.getAction());
+        sendRequest(method, jsonBody);
+    }
+    public  void sendVideo(long chat_id, String video_url) {
+        String method = "sendVideo";
+        String jsonBody = String.format("{\"chat_id\":\"%s\",\"video\":\"%s\"}", chat_id, video_url);
+        sendRequest(method, jsonBody);
+    }
+    public  void  getFile (String file_id) {
+        String method = "getFile";
+        String jsonBody = String.format("{\"file_id\":\"%s\"}", file_id);
+        sendRequest(method, jsonBody);
+    }
+    public void sendRequest(String method, String jsonBody) {
         try {
             httpClientService.sendPostRequest(method, jsonBody);
         } catch (Exception e) {
