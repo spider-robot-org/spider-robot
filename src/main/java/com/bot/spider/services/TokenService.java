e46666d390b7135f9ee665d9a95b8e54ddcbf3e2
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-02-14 18:16:45 -0300

    feat: add telegram request validation

--- src/main/java/com/bot/spider/services/TokenService.java
+++ src/main/java/com/bot/spider/services/TokenService.java
@@ -1,14 +1,19 @@
 package com.bot.spider.services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TokenService {
   @Value("${telegram.bot.secret-token}")
-  private static String secretToken;
-  public  static void validateToken(String token) {
+  private  String secretToken;
+
+  public  void validateToken(String token) {
+
     if(token.isEmpty()) {
       throw new IllegalArgumentException("Token is empty");
     }
+
     boolean isValid = token.equals(secretToken);
 
     if(!isValid) {
