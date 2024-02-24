a586b2c9a7bf71663276f2dacca229e98e2d5070
Jaobuenoh <joaobuenoh@hotmail.com>
2024-02-24 18:06:02 -0300

    Merge branch 'develop' into feature/14-enviroment-variable-owner

--- src/main/java/com/bot/spider/repository/UserRepository.java
+++ src/main/java/com/bot/spider/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.bot.spider.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    UserModel findByUsername (String username);
 }
---
+++ src/main/java/com/bot/spider/services/UserService.java
@@ -0,0 +1,70 @@
+package com.bot.spider.services;
+
+import com.bot.spider.enums.Role;
+import com.bot.spider.enums.UserStatus;
+import com.bot.spider.models.UserModel;
+import com.bot.spider.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+@Component
+public class UserService implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    private final ObjectMapper objectMapper;
+
+    @Value("${telegram.bot.owners}")
+    private String ownersJsonString;
+
+    @Autowired
+    public UserService(UserRepository userRepository, ObjectMapper objectMapper) {
+        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+            try {
+                List<Map<String, Object>> ownersFromFile = objectMapper.readValue(
+                        ownersJsonString,
+                        new TypeReference<List<Map<String, Object>>>() {
+                        }
+                );
+
+                for (Map<String, Object> ownerData : ownersFromFile) {
+                    String firstName = (String) ownerData.get("firstName");
+                    String lastName = (String) ownerData.get("lastName");
+                    String username = (String) ownerData.get("username");
+
+
+                    UserModel existingOwner = userRepository.findByUsername(username);
+
+                    if (existingOwner == null) {
+
+                        UserModel newOwner = new UserModel();
+                        newOwner.setFirstName(firstName);
+                        newOwner.setLastName(lastName);
+                        newOwner.setUsername(username);
+                        newOwner.setRole(Role.OWNER);
+                        newOwner.setStatus(UserStatus.ACTIVE);
+                        userRepository.save(newOwner); // Salvar novo proprietário
+
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error persisting data, or no data to persist, error: " + e);
+            }
+
+    }
+}
+
+
