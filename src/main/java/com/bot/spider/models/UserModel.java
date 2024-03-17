e8ac220f1cf422402f2866a3ffc0377cbbaed146
Jaobuenoh <joaobuenoh@hotmail.com>
2024-03-17 20:30:05 -0300

    refactor: add constructors and annotations to handle data

--- src/main/java/com/bot/spider/models/UserModel.java
+++ src/main/java/com/bot/spider/models/UserModel.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,16 +26,21 @@ public class UserModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "username can't be null")
     private String username;
 
+    @NotNull(message = "role can't be null")
     private Role role;
 
+    @NotBlank(message = "firstName can't be null")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotBlank(message = "lastName can't be null")
     @Column(name = "last_name")
     private String lastName;
 
+    @NotNull(message = "status can't be null")
     private UserStatus status;
 
     @Column(name = "accepted_terms_of_use")
@@ -47,6 +54,17 @@ public class UserModel {
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public UserModel(Long id, String username, Role role, String firstName, String lastName, UserStatus status, boolean acceptedTermsOfUse, boolean acceptedPrivacyPolicy) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.status = status;
+        this.acceptedTermsOfUse = acceptedTermsOfUse;
+        this.acceptedPrivacyPolicy = acceptedPrivacyPolicy;
+    }
 
     @PrePersist
     protected void onCreate() {
